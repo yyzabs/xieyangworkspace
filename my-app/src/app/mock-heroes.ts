52ffb9b5c0c1514e05d586947f0b22fc2c604ea3
xieyang <214659547@qq.com>
2018-09-04 23:44:34 +0800

    add a file in app directory

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id:11,name:'Mr.Nice'},
+  {id:12,name:'Wang Qing Yan'},
+  {id:13,name:'Xie Yang'},
+  {id:14,name:'Mr.Xu'},
+  {id:15,name:'Tina'},
+  {id:16,name:'Magma'},
+  {id:17,name:'Xue Xuan'},
+  {id:18,name:'Dynama'},
+  {id:19,name:'Xu Jiang Qiao'},
+  {id:20,name:'Narco'},
+];
